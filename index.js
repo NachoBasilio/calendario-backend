347bdcbf7d496b9b8c5390b6743eee2041472395
IgnaDev <arcortoon@gmail.com>
2023-09-04 17:23:31 -0300

    Primer servidor express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola gente")
+const express = require('express');
+
+const app = express()
+
+
+app.get("/", (req, res)=>{
+    console.log("Se requiere el /")
+    res.json({
+        ok:true
+    })
+})
+
+
+app.listen(4002, ()=>{
+    console.log("Servidor corriendo en puerto " + 4002)
+})
