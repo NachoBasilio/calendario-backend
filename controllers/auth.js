5e14ebd9883320543d02f0b867902938fe92ad3e
IgnaDev <arcortoon@gmail.com>
2023-09-06 17:57:52 -0300

    Manejo de errores

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 const {response} = require("express")
+const {validationResult} = require("express-validator")
 
 const crearUsuario = (req, res = response)=>{
 
     const {name, email, password} = req.body
 
-    if(name.length < 5){
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
         return res.status(400).json({
             ok:false,
-            msg: "El nombre debe de ser de 5 letras"
+            errors: errors.mapped()
         })
     }
 
@@ -22,6 +24,14 @@ const crearUsuario = (req, res = response)=>{
 
 const logearUsuario = (req, res = response)=>{
     const {email, password} = req.body 
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok:false,
+            errors: errors.mapped()
+        })
+    }
 
     res.json({
         ok:true,
--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,27 @@ const { crearUsuario, logearUsuario, recargarCredenciales } = require("../contro
 
 
 const {Router} = require("express")
+const {check} = require("express-validator")
 const router = Router()
 
-router.post("/",logearUsuario )
+router.post(
+    "/",
+    [
+        check("email", "El email es obligatorio").isEmail(),
+        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6})
+    ],
+    logearUsuario 
+)
 
-router.post("/new", crearUsuario)
+router.post(
+    "/new", 
+    [
+        check("name", "El nombre es obligatorio").not().isEmpty(),
+        check("email", "El email es obligatorio").isEmail(),
+        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6})
+    ],
+    crearUsuario
+)
 
 router.get("/renew", recargarCredenciales)
 
