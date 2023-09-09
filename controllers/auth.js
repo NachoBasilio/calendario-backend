183e827471296354c6f500e3652960b51356ea06
IgnaDev <arcortoon@gmail.com>
2023-09-08 21:15:46 -0300

    Aplique express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,13 +5,6 @@ const crearUsuario = (req, res = response)=>{
 
     const {name, email, password} = req.body
 
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(201).json({
         ok:true,
@@ -25,13 +18,7 @@ const crearUsuario = (req, res = response)=>{
 const logearUsuario = (req, res = response)=>{
     const {email, password} = req.body 
 
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        })
-    }
+
 
     res.json({
         ok:true,
--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,15 @@ const { crearUsuario, logearUsuario, recargarCredenciales } = require("../contro
 
 const {Router} = require("express")
 const {check} = require("express-validator")
+const { validarCampos } = require("../middlewares/validar-campos")
 const router = Router()
 
 router.post(
     "/",
     [
         check("email", "El email es obligatorio").isEmail(),
-        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6})
+        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6}),
+        validarCampos
     ],
     logearUsuario 
 )
@@ -24,7 +26,8 @@ router.post(
     [
         check("name", "El nombre es obligatorio").not().isEmpty(),
         check("email", "El email es obligatorio").isEmail(),
-        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6})
+        check("password", "El password debe tener al menos 6 caracteres").isLength({min:6}),
+        validarCampos
     ],
     crearUsuario
 )
