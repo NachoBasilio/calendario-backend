db807d0d2cd1d5d518ef53bb734aeb1f43d51591
IgnaDev <arcortoon@gmail.com>
2023-09-11 17:39:33 -0300

    Validada la fecha

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,14 @@
 
 */
 const { Router } = require('express');
+const {check} = require("express-validator")
+const { validarCampos } = require('../middlewares/validar-campos');
+
+
 
 const { validarJWT } = require("../middlewares/validar-jwt")
-const {getEventos,crearEvento,actualizarEvento,eliminarEvento} = require("../controllers/events")
+const {getEventos,crearEvento,actualizarEvento,eliminarEvento} = require("../controllers/events");
+const { isDate } = require('../hellpers/isDate');
 
 
 const router = Router()
@@ -15,7 +20,14 @@ router.use(validarJWT)
 
 
 router.get("/", getEventos)
-router.post("/", crearEvento)
+router.post("/", 
+[
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de final es obligatoria").custom(isDate),
+    validarCampos
+]
+,crearEvento)
 router.put("/", actualizarEvento)
 router.delete("/", eliminarEvento)
 
